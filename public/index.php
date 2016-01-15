c2917c6e47ae40e75873b7a378014d455ce21418
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 01:35:26 -0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
-	
 	// load functions
     require('../functions/functions.php');
 
-
 	// this is the main controller that handle the request from URL
-    if (isset($_GET['para1']))
-    {
+    if (isset($_GET['para1'])) {
         $cat   = $_GET['para2'];
         $cat   = str_replace('-', ' ', $cat);
         $items = query_items($cat);
@@ -15,8 +12,7 @@
         load_html('category', ['items' => $items]);
         load_html('footer');
     }
-    else
-    {
+    else {
         load_html('header');
         load_html('index');
         load_html('footer');
