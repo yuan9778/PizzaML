9060db004cd16f44632a3f8ff99a849de2cd1bbf
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 04:03:04 -0800

    Update empty_cart.php

--- views/empty_cart.php
+++ views/empty_cart.php
@@ -1,3 +1,5 @@
-<div class="order-format">
-  <h2>Cart is empty!</h2>
-</div>
+        <div class="col-md-9 order-format">
+            <h2>Cart is empty!</h2>
+        </div>
+    </div> <!--row-->
+</div> <!--container-->
