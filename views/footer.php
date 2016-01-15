43552224c3931622c723c73c6ece0e13aa1f751d
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 01:41:07 -0800

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,10 +1,10 @@
 
     <footer class="fixed">
-		<div class="container">
-		   <p>This is a fake website for ordering pizza and other food. This is projetc0 of Harvard summer class <a href="http://cs75.tv/2012/summer/#about,projects">cs-75: buiding dynamic webwites</a>.</p>
-		   <p>More info at <a href="https://github.com/yuan9778/Online-Pizza-Order-website">https://github.com/yuan9778/Online-Pizza-Order-website</a>.</p>
-		   <p>&copy; SQY - 2015</p>
-		 </div>
+	<div class="container">
+	    <p>This is a fake website for ordering pizza and other food. This is projetc0 of Harvard summer class <a href="http://cs75.tv/2012/summer/#about,projects">cs-75: buiding dynamic webwites</a>.</p>
+            <p>More info at <a href="https://github.com/yuan9778/Online-Pizza-Order-website">https://github.com/yuan9778/Online-Pizza-Order-website</a>.</p>
+            <p>&copy; SQY - 2015</p>
+	</div>
      </footer>
      
   </body>
