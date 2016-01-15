a076367b8ce16eca10a0c8c491025b2247aac606
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 01:37:51 -0800

    Update order.php

--- public/order.php
+++ public/order.php
@@ -1,82 +1,66 @@
 <?php
 
-// this is another controller file handling orders (merging the same order, add new order to sessions)
+    // this is another controller file handling orders (merging the same order, add new order to sessions)
 
     require('../functions/functions.php');
     require('../models/model.php');
 
     // if form was submitted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // validate submission
-        if (empty($_POST['quantity']))
-        {
+        if (empty($_POST['quantity'])) {
             error_message('please specify a quantity');
         }
         // quantity must be a positive integer
-        else if (!preg_match('/^\d+$/', $_POST['quantity']))
-        {
+        else if (!preg_match('/^\d+$/', $_POST['quantity'])) {
             error_message('quantity must be a positive integer');
         }
         // validate quantity range
-        else if ($_POST['quantity'] < 1 || $_POST['quantity'] > MAX_QUANT)
-        {
+        else if ($_POST['quantity'] < 1 || $_POST['quantity'] > MAX_QUANT) {
             error_message('quantity must be between 1 and ' . MAX_QUANT);
         }
 
         $existing_order = False;
 
         // update current order quantity if exist
-        if (!empty($_SESSION['orders']))
-        {
+        if (!empty($_SESSION['orders'])) {
             // search through orders
-            foreach ($_SESSION['orders'] as $id => $order)
-            {
+            foreach ($_SESSION['orders'] as $id => $order) {
                 // handle different sizes
-                if (isset($_POST['size']))
-                {
-                    if ($_POST['item'] == $order['item'] && $_POST['size'] == $order['size'])
-                    {
+                if (isset($_POST['size'])) {
+                    if ($_POST['item'] == $order['item'] && $_POST['size'] == $order['size']) {
                         $existing_order = True;
 
                         // if limit is already reached
-                        if ($_SESSION['orders'][$id]['quantity'] >= MAX_QUANT)
-                        {
+                        if ($_SESSION['orders'][$id]['quantity'] >= MAX_QUANT) {
                             error_message("you can't order more than " . MAX_QUANT . " of that item");
                         }
-                        else
-                        {
+                        else {
                             // increment quantity
                             $_SESSION['orders'][$id]['quantity'] += $_POST['quantity'];
                         }
 
                         // limit quantity
-                        if ($_SESSION['orders'][$id]['quantity'] > MAX_QUANT)
-                        {
+                        if ($_SESSION['orders'][$id]['quantity'] > MAX_QUANT) {
                             $_SESSION['orders'][$id]['quantity'] = MAX_QUANT;
                         }
                     }
                 }
-                else
-                {
-                    if ($_POST['item'] == $order['item'])
-                    {
+                else {
+                    if ($_POST['item'] == $order['item']) {
                         $existing_order = True;
 
                         // if limit is already reached
-                        if ($_SESSION['orders'][$id]['quantity'] >= MAX_QUANT)
-                        {
+                        if ($_SESSION['orders'][$id]['quantity'] >= MAX_QUANT) {
                             error_message("you can't order more than " . MAX_QUANT . "&nbsp; items");
                         }
-                        else
-                        {
+                        else {
                             // increment quantity
                             $_SESSION['orders'][$id]['quantity'] += $_POST['quantity'];
                         }
 
                         // limit quantity
-                        if ($_SESSION['orders'][$id]['quantity'] > MAX_QUANT)
-                        {
+                        if ($_SESSION['orders'][$id]['quantity'] > MAX_QUANT) {
                             $_SESSION['orders'][$id]['quantity'] = MAX_QUANT;
                         }
                     }
@@ -85,8 +69,7 @@
         }
 
         // find correct price
-        if (isset($_POST['small']) || isset($_POST['large']))
-        {
+        if (isset($_POST['small']) || isset($_POST['large'])) {
             $_POST['price'] = $_POST[$_POST['size']];
         }
 
@@ -94,8 +77,7 @@
         $id = uniqid();
 
         // if the order is new, store order in session
-        if (!$existing_order)
-        {
+        if (!$existing_order) {
             $_SESSION['orders'][$id] = $_POST;
         }
 
