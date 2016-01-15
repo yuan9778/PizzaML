720244f538f012a6440bdbd952a75307df5a813a
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 04:08:11 -0800

    Update order.php

--- views/order.php
+++ views/order.php
@@ -1,5 +1,6 @@
-<div class="order-format">
-  <h2>The item was added to the cart!</h2>
-
-  <p>Go <a href="javascript:history.go(-1)">back</a> or to the <a href="/shopping_cart.php">Shopping Cart</a>.</p>
-</div><!-- .message -->
+        <div class="col-md-9 order-format">
+            <h2>The item was added to the cart!</h2>
+            <p>Go <a href="javascript:history.go(-1)">back</a> or to the <a href="/shopping_cart.php">Shopping Cart</a>.</p>
+        </div><!--col-md-9-->
+    </div><!--row-->
+</div><!--container-->
